511e19ec963d265c3f87c842eac7918efa0b716c
Darrell803 <35411609+Darrell803@users.noreply.github.com>
2019-12-22 13:39:18 -0500

    Add files via upload

---
+++ leetspeak1.py
@@ -0,0 +1,13 @@
+
+print('Enter some text:')
+text = input()
+
+leet = text.replace('a', '4')
+leet = leet.replace('b', '8')
+leet = leet.replace('e', '3')
+leet = leet.replace('i', '1')
+leet = leet.replace('o', '0')
+leet = leet.replace('s', '5')
+leet = leet.replace('t', '7')
+
+print(leet)
